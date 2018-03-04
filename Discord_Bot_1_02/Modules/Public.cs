3bf2ff31d6efb10164e9a238971c9d545bd359b4
rniinim <r.niinimakiwindowslive.com>
2018-03-04 19:15:02 +0200

    On branch master

---
+++ Discord_Bot_1_02/Modules/Public.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Discord_Bot_1_02.Modules
+{
+    public class Public : ModuleBase<SocketCommandContext>
+    {
+        Random rng = new Random();
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Hello");
+        }
+        
+        
+
+        [Command("roulette")]
+        public async Task RouletteAsync()
+        {
+            
+            await ReplyAsync(rng.Next(1,7) == 6 ? "Boom" : "Click");
+        }
+    }
+}
